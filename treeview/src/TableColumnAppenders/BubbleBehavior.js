4f84267e87ed10b08bf9a1c3ebeff73beab2c743
Namyts <35004248+Namyts@users.noreply.github.com>
2021-10-18 16:01:25 +0100

    removed all apart from themedesigner

--- treeview/src/TableColumnAppenders/BubbleBehavior.js
+++
@@ -1,5 +0,0 @@
-export const getColourFromMap = (lookup,map)=>{
-	let colourindex = map.map(el=>el[0]).indexOf(lookup)
-	let displaycolour = colourindex!==-1 ? map[colourindex][1] : 'white'
-	return displaycolour
-}
